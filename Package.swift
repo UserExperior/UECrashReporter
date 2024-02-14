a4e6ca6ddd9d7a8abf65a07f64ae99337944170b
Shekhar Gupta <shekhar.gupta@devrev.ai>
2024-02-14 13:55:23 +0530

    Making Library type as "static"

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "1.0.6"
+let version = "1.0.7"
 let checksum = "d75dc55eb20139713b8649ddea0d7b323fdd69116e39da2212dc5fca93d730c8"
 let repoName = "UECrashReporter"
 
@@ -14,6 +14,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "UECrashReporter",
+            type: .static,
             targets: ["UECrashReporterWrapper", "UECrashReporter"]
         ),
     ],
